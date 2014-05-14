a00d677038dfbd57308d0107b1fb6ed67f3c9247
Chris Chares <chris@eunoia.cc>
2014-05-13 23:16:59 -0600

    Initial Commit

---
+++ sails.ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sails.ios
+//
+//  Created by Chris on 5/13/14.
+//  Copyright (c) 2014 eunoia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sails.ios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sails.ios
+//
+//  Created by Chris on 5/13/14.
+//  Copyright (c) 2014 eunoia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
